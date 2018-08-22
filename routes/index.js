29b64a098c66492bbf4954f6010c1a015f4bed33
Louis Knapp <louisknapp@gmail.com>
2018-08-22 15:21:30 -0500

    lk | iterating through json blob that gets imported from models dir

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var hires = require('../models/newHires');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    newHiresByDate: hires
+  });
 });
 
 module.exports = router;
